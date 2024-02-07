b209b7962a80511a174a3fb7bd0e16cf94213b32
Kevin SIMON <simonkvin@gmail.com>
2024-02-07 17:48:56 +0100

    correcting script and style

--- my_bot/bot_api.py
+++ my_bot/bot_api.py
@@ -16,7 +16,6 @@ url = "https://api.edenai.run/v2/text/chat"
 # Scraping all the text of my website and remove the leading and trailing whitespaces with strip()
 response = requests.get("http://localhost:8001")
 soup = BeautifulSoup(response.content, "html.parser")
-site_content = soup.get_text().strip()
 
 app = FastAPI()
 
@@ -42,7 +41,7 @@ async def bot_request(prompt):
     payload = {
         "providers": provider,
         "text": "",
-        "chatbot_global_action": f"Act as Kevin, the owner of the website of which here is the content : {site_content}",
+        "chatbot_global_action": f"You are not an assistant. You are Kevin, the owner of the website of which here is the content : {site_content}",
         "previous_history": [],
         "temperature": 0.0,
         "max_tokens": 150,
