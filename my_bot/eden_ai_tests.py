e5eca9822d7e5efe150e23cf4688ba0e54cd8081
Kevin SIMON <87190254+kstarkiller@users.noreply.github.com>
2024-02-07 17:58:40 +0100

    Update and rename bot_test_talks.py to eden_ai_tests.py

--- my_bot/eden_ai_tests.py
+++ my_bot/eden_ai_tests.py
@@ -1,6 +1,5 @@
 import json
 import requests
-from bs4 import BeautifulSoup
 import sys
 sys.path.append("../../")
 from hidden import *
@@ -9,11 +8,6 @@ from hidden import *
 headers = {"Authorization": "Bearer " + B09_API_KEY}
 provider = "openai"
 url = "https://api.edenai.run/v2/text/chat"
-
-# Find all the text of my website and remove the leading and trailing whitespaces with strip()
-response = requests.get("http://localhost:8001")
-soup = BeautifulSoup(response.content, "html.parser")
-text = soup.get_text().strip()
 
 # Define a function to get the bot response
 def get_bot_response(provider, url, headers):
